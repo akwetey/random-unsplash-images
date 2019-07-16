4e80cda7735875a046f586297fd55259a00ebd2f
akwetey <okinejonathan@yahoo.com>
2019-07-16 22:48:46 +0100

    aysnc await on promises

--- resources/app.js
+++ resources/app.js
@@ -5,17 +5,12 @@ const collectionId = 1160922;
 const imagesAvailable = 141;
 const gallery = document.querySelector('.gallery-container')
 
-function renderImages(randomNumber) {
-    fetch(`https://source.unsplash.com/collection/${collectionId}/${imagewidth}x${imageheight}/?sig=${randomNumber}`)
-        .then((response) => {
-            let galleryImage = document.createElement('div');
-            galleryImage.classList.add('gallery-image');
-            galleryImage.innerHTML = `<img class="gallery-image" src="${response.url}" alt="random images"></img>`
-            gallery.appendChild(galleryImage)
-        })
-        .catch((error) => {
-            console.log(error.response)
-        })
+async function renderImages(randomNumber) {
+    const response = await fetch(`https://source.unsplash.com/collection/${collectionId}/${imagewidth}x${imageheight}/?sig=${randomNumber}`)
+    let galleryImage = document.createElement('div');
+    galleryImage.classList.add('gallery-image');
+    galleryImage.innerHTML = `<img class="gallery-image" src="${response.url}" alt="random images"></img>`
+    gallery.appendChild(galleryImage)
 }
 for (let i = 0; i < numberOfImages; i++) {
     let randomImageIndex = Math.floor(Math.random() * imagesAvailable);
